249ff6f981d0115219ab014b76602318dd7b2066
Flash <btovt@rambler.ru>
2023-04-03 13:59:55 +0300

    added function to get fresh news

--- botttt.py
+++ botttt.py
@@ -1,7 +1,7 @@
 # импорт функции для поддержки работоспособности
 # from background import keep_alive
 from threading import Thread
-from parser_1 import get_info
+from parser_1 import get_info, get_fresh_news
 from random import randrange
 import time
 import json
@@ -18,53 +18,18 @@ dp = Dispatcher(bot)
 async def send_news(user_id):
 
     global article
-    global new_state
-    global prev_art_date
-    new_state = False
-    zakrep_published = False
-
-    with open('artile_info.json', 'r') as f:
-        article = json.load(f)
-    if article[0]['zakrep'] != "":
-        zakrep_published = True
-        prev_zakrep_date = article[0]['date_pub']
-
-    out_to_user = ''
-    out_to_user=f"_Новость от {article[0]['date_pub']}_\n\n*{article[0]['title']}\n\n*{article[0]['text']}{'-'*25}\n\n"
-    
-    await bot.send_photo(user_id, article[0]['img_art'])
-    await bot.send_message(user_id, out_to_user, parse_mode='Markdown')  
-
-    prev_art_date = article[0]['date_pub']
 
     while (1):
-        get_info('https://dota2.ru/news/')
+        articles = get_fresh_news('https://dota2.ru/news/')
         await asyncio.sleep(randrange(30, 60))
-        with open('artile_info.json', 'r') as f:
-            article = json.load(f)
-        if article[0]['zakrep'] != "" and article[0]['date_pub'] != prev_zakrep_date:
+        for article in articles:
+            art_id = list(article.keys())
 
-            zakrep_published == False
-        if article[0]['date_pub'] != prev_art_date and article[0]['zakrep'] == "":
-
-            new_state = True
-        elif article[0]['zakrep'] != "" and article[0]['date_pub'] != prev_zakrep_date and zakrep_published == False:
-
-            new_state = True
-            prev_zakrep_date = article[0]['date_pub']
-        elif zakrep_published == True and article[0]['zakrep'] != "" and article[0]['date_pub'] == prev_zakrep_date and article[1]['date_pub'] != prev_art_date:
-
-            article[0], article[1] = article[1], article[0]
-            new_state = True
-
-        if new_state:
             out_to_user = ''
-            out_to_user=f"_Новость от {article[0]['date_pub']}_\n\n*{article[0]['title']}\n\n*{article[0]['text']}{'-'*25}\n\n"
-            
-            await bot.send_photo(user_id, article[0]['img_art'])
-            await bot.send_message(user_id, out_to_user, parse_mode='Markdown')  
-            prev_art_date = article[0]['date_pub']
-            new_state = False
+            out_to_user = f"_Новость от {article[art_id]['date_pub']}_\n\n*{article[art_id]['title']}\n\n*{article[art_id]['text']}{'-'*25}\n\n"
+
+            await bot.send_photo(user_id, article[art_id]['img_art'])
+            await bot.send_message(user_id, out_to_user, parse_mode='Markdown')
 
 
 # Явно указываем в декораторе, на какую команду реагируем.
@@ -75,13 +40,13 @@ async def send_welcome(message: types.Message):
     # Так как код работает асинхронно, то обязательно пишем await.
     await message.reply(f"Привет {user_full_name}!\nЯ новостной бот по дота 2, буду скидывать вам новости по мере их появления.")
     start_buttons = ["Получать свежие новости", "Последние 5 новостей"]
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_buttons)
-    await message.answer("Лента новостей", reply_markup = keyboard )
+    await message.answer("Лента новостей", reply_markup=keyboard)
 
 
 # Явно указываем в декораторе, на какую команду реагируем.
-@dp.message_handler(Text(equals = ["Получать свежие новости"]))
+@dp.message_handler(Text(equals=["Получать свежие новости"]))
 async def get_news(message: types.Message):
     get_info('https://dota2.ru/news/')
     global user_id
@@ -94,19 +59,20 @@ async def get_news(message: types.Message):
     # Thread(target = if_new_state, args = (new_state, prev_art_date,)).start()
 
 
-@dp.message_handler(Text(equals = ["Последние 5 новостей"]))
+@dp.message_handler(Text(equals=["Последние 5 новостей"]))
 async def last_5_news(message: types.Message):
     get_info('https://dota2.ru/news/')
     user_id = message.from_user.id
     await bot.send_message(user_id, "Вот последние 5 новостей \U0001F60E")
     with open('artile_info.json', 'r') as f:
-            article = json.load(f)
-    for i in range(0,5):
+        article = json.load(f)
+    articles_keys = list(article)[:5]
+    for key in articles_keys:
         out_to_user = ''
-        out_to_user=f"_Новость от {article[i]['date_pub']}_\n\n*{article[i]['title']}\n\n*{article[i]['text']}{'-'*25}\n\n"
-        
-        await bot.send_photo(user_id, article[i]['img_art'])
-        await bot.send_message(user_id, out_to_user, parse_mode='Markdown')    
+        out_to_user = f"_Новость от {article[key]['date_pub']}_\n\n*{article[key]['title']}\n\n*{article[key]['text']}{'-'*25}\n\n"
+
+        await bot.send_photo(user_id, article[key]['img_art'])
+        await bot.send_message(user_id, out_to_user, parse_mode='Markdown')
 
 
 if __name__ == '__main__':
--- parser_1.py
+++ parser_1.py
@@ -20,18 +20,15 @@ def get_info(url=''):
     # pagination_count = int(soup.find("ul", class_ = 'pagination').find('li', class_ = 'pagination__item pagination__link--right').text)
     
     articles_urls_list = []
-    d = []
+    d = {}
     for page in range(1, 2):
         response = s.get(f'https://dota2.ru/news/?page={page}', headers = headers)
         soup = BeautifulSoup(response.text, 'lxml')
         articles_urls = soup.find_all('a', class_='index__news-link game-icon js-game-icon')
-        try:
-            zakrep = soup.find('div', class_ = 'gradient-btn index__news-block-pinned').text.strip()
-        except AttributeError:
-            zakrep = ""
         
         for i, art_url in enumerate(articles_urls):
             art_url = 'https://dota2.ru'+art_url.get('href')
+            art_id = art_url.split('-')[0].split('/')[-1]
             
             # articles_urls_list.append(art_url)
             response = s.get(art_url,headers = headers)
@@ -51,37 +48,76 @@ def get_info(url=''):
             # response = s.get(url = img_art)
             # with open(f'./imgs/{img_art.split("/")[-1]}','wb') as file:
             #     file.write(response.content)
-            if i==0:
-                d.append(
-                    {
-                        'original_url':art_url,
-                        'title': title,
-                        'date_pub': date_pub,
-                        'text':text_str,
-                        'img_art':img_art,
-                        'zakrep':zakrep
-                    }
-                )
-            else:
-                d.append(
-                    {
-                        'original_url':art_url,
-                        'title': title,
-                        'date_pub': date_pub,
-                        'text':text_str,
-                        'img_art':img_art,
-                        'zakrep':""
-                    }
-                )
-
+            d[art_id]={
+                    'original_url':art_url,
+                    'title': title,
+                    'date_pub': date_pub,
+                    'text':text_str,
+                    'img_art':img_art,
+                    
+                }
 
     with open('artile_info.json','w') as file:
             json.dump(d, file, indent = 4, ensure_ascii=False)
          
+def get_fresh_news(url = ''):
+    with open('artile_info.json','r') as f:
+         list_of_articles = json.load(f)  
+    s = requests.Session()
+    new_articles = {}
+    for page in range(1, 2):
+        response = s.get(f'https://dota2.ru/news/?page={page}', headers = headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+        articles_urls = soup.find_all('a', class_='index__news-link game-icon js-game-icon')
+
+        for i, art_url in enumerate(articles_urls):
+            art_url = 'https://dota2.ru'+art_url.get('href')
+            art_id = art_url.split('-')[0].split('/')[-1]
+
+            if art_id in list_of_articles:
+                continue
+            else:
+                response = s.get(art_url,headers = headers)
+                soup = BeautifulSoup(response.text, 'lxml')
+                
+                
+                title = soup.find('h1', class_ = 'title-global').text.strip()
+                date_pub = soup.find('p', class_ = 'forum-theme__top-block-time').text.strip()
+                text = soup.find('div', class_ = 'bg-main-block').find('div', class_  = 'global-content p-content js-text-errors-wrap').find_all('p')
+                text = [block.text.replace('\xa0',' ') for block in text]
+                text_str = '' # Текст статьи
+                for txt in text:
+                    if txt!='':
+                        text_str+=(txt.strip().replace('. ','.')+'\n\n')
+                text = text_str
+                img_art = soup.find('div', class_ = 'global-content p-content js-text-errors-wrap').find('img')["src"]
+            
+                new_articles[art_id]={
+                        'original_url':art_url,
+                        'title': title,
+                        'date_pub': date_pub,
+                        'text':text_str,
+                        'img_art':img_art,
+                        
+                    }
+                list_of_articles[art_id]={
+                        'original_url':art_url,
+                        'title': title,
+                        'date_pub': date_pub,
+                        'text':text_str,
+                        'img_art':img_art,
+                        
+                    }
+    with open('artile_info.json','w') as f:
+         json.dump(list_of_articles, f, indent = 4, ensure_ascii=False)             
+    
+    return new_articles
+
 
 def main():
     get_info('https://dota2.ru/news/')
-    # get_info_article('urls_list')
+    get_fresh_news('https://dota2.ru/news/')
+    
 
 if __name__=='__main__':
     main()
